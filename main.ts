7a9c8baedc8fe64b9413c32fffd85f3e2becae98
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-12-24 17:27:49 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,35 @@
  * This program turns lights on and off
 */
 
+
+input.onButtonPressed(Button.A, function () {
+basic.showString("CHRISTMAS LIGHTS!")
+
+//setup 
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1) 
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+//start of show 
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+
+
+
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+
+
+
+})
+
