b8679ae528e85b07978459a1e12018291cba0cc7
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-12-24 18:00:45 -0500

    works

--- main.ts
+++ main.ts
@@ -5,35 +5,67 @@
  * This program turns lights on and off
 */
 
-
-input.onButtonPressed(Button.A, function () {
-basic.showString("CHRISTMAS LIGHTS!")
-
 //setup 
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    pins.digitalWritePin(DigitalPin.P9, 1)
-    pins.digitalWritePin(DigitalPin.P13, 1) 
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P9, 0)
-    pins.digitalWritePin(DigitalPin.P13, 0)
-//start of show 
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P5, 0)
-    pins.digitalWritePin(DigitalPin.P9, 1)
-    pins.digitalWritePin(DigitalPin.P9, 0)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+
+input.onButtonPressed(Button.B, function () {
+
+pins.digitalWritePin(DigitalPin.P5, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P5, 0)
+pins.digitalWritePin(DigitalPin.P9, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P9, 0)
+pins.digitalWritePin(DigitalPin.P13, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P13, 0)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P13, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P9, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P9, 0)
+pins.digitalWritePin(DigitalPin.P5, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P5, 0)
+basic.pause(550)
+
+//all on 
+pins.digitalWritePin(DigitalPin.P5, 1)
+pins.digitalWritePin(DigitalPin.P9, 1)
+pins.digitalWritePin(DigitalPin.P13, 1)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P5, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
+basic.pause(550)
+pins.digitalWritePin(DigitalPin.P9, 0)
+pins.digitalWritePin(DigitalPin.P5, 1)
+pins.digitalWritePin(DigitalPin.P13, 1)
+basic.pause(550)
+
+//cool 
+pins.digitalWritePin(DigitalPin.P9, 0)
+pins.digitalWritePin(DigitalPin.P5, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
+basic.pause(300)
+pins.digitalWritePin(DigitalPin.P5, 1)
+basic.pause(300)
+pins.digitalWritePin(DigitalPin.P5, 0)
+basic.pause(300)
+pins.digitalWritePin(DigitalPin.P13, 1)
+basic.pause(300)
+pins.digitalWritePin(DigitalPin.P13, 0)
+basic.pause(300)
+pins.digitalWritePin(DigitalPin.P9, 1)
+basic.pause(250)
+pins.digitalWritePin(DigitalPin.P5, 1)
+pins.digitalWritePin(DigitalPin.P13, 1)
+basic.pause(500)
+pins.digitalWritePin(DigitalPin.P9, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P5, 0)
 
 
-
-
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    pins.digitalWritePin(DigitalPin.P5, 1)
-
-
-
+basic.showString("CHRISTMAS!")
+music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
 })
-
